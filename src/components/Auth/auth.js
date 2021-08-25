4137ad5a5b2077fd29284eea2701b82169b5344f
Akbar Hazura <akbarhazura@gmail.com>
2021-08-25 09:51:13 +0700

    urlready

--- src/components/Auth/auth.js
+++ src/components/Auth/auth.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = "f7b78cf34ebd42b0987a3be6d8600c45";
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
-const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/";
+const REDIRECT_URL_AFTER_LOGIN = "https://spotidev.vercel.app/";
 const SPACE_DELIMITER = "%20";
 const SCOPE = ["user-read-email","playlist-modify-private","user-read-private","user-library-modify","user-library-read","streaming","user-read-playback-state","user-modify-playback-state"];
 const SCOPE_URL_PARAM = SCOPE.join(SPACE_DELIMITER)
